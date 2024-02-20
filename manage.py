6df487d4f52bd6d0354011e9187206b98fe19c9a
Ubuntu <ubuntu@ip-172-31-18-202.eu-north-1.compute.internal>
2024-02-20 05:07:52 +0000

    server

--- manage.py
+++ manage.py
@@ -1,12 +1,20 @@
-from flask.cli import FlaskGroup
+#from flask.cli import FlaskGroup
 
+#from src import app
+
+#cli = FlaskGroup(app)
+
+
+#if __name__ == "__main__":
+#   cli()
+
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0', port=8000, debug=True)
+from flask.cli import FlaskGroup
 from src import app
 
 cli = FlaskGroup(app)
 
+if __name__ == "__main__":
+    cli()
 
-# if __name__ == "__main__":
-#     cli()
-
-if __name__ == '__main__':
-    app.run(host='192.168.200.98', port=5000, debug=True)
